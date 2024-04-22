1f390f9276f9c8a21a398966c7070101542ed7f5
Zhiyuan Jiang <zhiyuanj@andrew.cmu.edu>
2024-04-22 01:27:28 -0400

    initial commit via upload

---
+++ eureka-fastpass-service-demo/src/test/java/com/demo/EurekaFastpassServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaFastpassServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
